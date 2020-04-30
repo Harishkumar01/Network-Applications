cfb50876de02119c4cfc6c304819546cd240e4bf
Harish <harishkumarvijayarajan@gmail.com>
2020-04-30 21:29:00 +0530

    pop mail

---
+++ popmail.py
@@ -0,0 +1,22 @@
+#install popa3d //sudo apt install popa3d
+#mail -s //to ur local machine
+#sudo service popa3d start
+
+
+import poplib
+from email.message import EmailMessage
+
+server = "127.0.0.1"
+user = "harish-kumar"
+passwd = "vharishk"
+
+server = poplib.POP3(server)
+server.user(user)
+server.pass_(passwd)
+
+msgcount = len(server.list()[1])
+
+for i in range(msgcount):
+	for msg in server.retr(i+1)[1]:
+		print(msg.decode())
+
