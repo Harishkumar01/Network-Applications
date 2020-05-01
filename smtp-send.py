8212dc0b7227c4d9e5ef10d788c483d81a70fe14
Harish <harishkumarvijayarajan@gmail.com>
2020-05-01 11:54:30 +0530

    sample code of smtp

---
+++ smtp-send.py
@@ -0,0 +1,22 @@
+#run an smtp server in local
+
+import smtplib
+
+message = """From: From harish <harishvijayarajan@gmail.com>
+To: To Harishkumar <machine running smtp server>
+MIME-Version: 1.0
+Content-Type: text/html
+Subject: Test HTML email
+
+This is an html email message
+
+
+<h1>this is test HTML Message</h1>
+"""
+
+try:
+	smtp = smtplib.SMTP("ip of smtp server")
+	smtp.sendmail("from, to, message")
+	print("Email sent")
+except Exception as err:
+	print(err)
